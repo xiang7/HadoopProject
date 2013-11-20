b9e0fb3fa4a87bba014fd3d2afd986d11981a2c5
xiang7 <xiang7@purdue.edu>
2013-11-20 11:47:02 -0500

    data cleaning steps

---
+++ DataCleaning.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.utils.*;
+
+public class DataCleaning{
+	public static void main(String args[]){
+		
+	}
+
+	/*
+		String s - one record
+		this method cleans s and return it
+	******************************************
+		Cleaning steps:
+		1. 
+	*/
+	public static String clean(String s){
+		
+	}
+}
